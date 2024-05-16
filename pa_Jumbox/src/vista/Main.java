6bdbf48a450bf081014ed69dcb3aa6dc13a08772
gonzavadone <gonzalo.vadone@davinci.edu.ar>
2024-05-16 15:20:48 -0300

    cambioss

--- pa_Jumbox/src/vista/Main.java
+++ pa_Jumbox/src/vista/Main.java
@@ -17,6 +17,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
+		
 		BaseDatos bd = new BaseDatos();
 		
 		bd.getAlmacenistas().add(new Almacenista(2, "Ana", "López"));
